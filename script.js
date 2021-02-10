944d06813c09ac42b7ba434e17a91ec23b62809c
Kathrine <kath423a@stud.kea.dk>
2021-02-10 09:54:50 +0100

    array loop

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+window.addEventListener("DOMContentLoaded", loop);
+
+const arr = [];
+let counting = 0;
+
+function loop() {
+  arr.unshift(counting);
+  counting++;
+  if (counting > 9) {
+    arr.pop(arr);
+  }
+
+  setTimeout(loop, 500);
+
+  console.log(arr);
+}
